1f93a14f96a75bb2540917f5ae79d56020232db6
cubernetes <tischmid@proton.me>
2024-08-03 17:59:38 +0200

    Make check_todo.sh script more lax

--- check_todo.sh
+++ check_todo.sh
@@ -2,4 +2,4 @@
 
 # TODO: ONLY remove this file when the following command doesn't print anything anymore
 # (except this file) of course
-grep --color=always --exclude-dir=.git --exclude-dir=libft -inr -- 'TODO[:]'
+grep --color=always --exclude-dir=.git --exclude-dir=libft -inr -- 'TODO'
